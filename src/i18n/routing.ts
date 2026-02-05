c6545ce3b42be1fdad0025e92840568afc9d3548
Sophie Adam <sophieadam@Sophies-MacBook-Air.local>
2026-02-05 16:06:01 +0100

    Add localePrefix: 'always' to routing config for Vercel compatibility

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,15 +1,18 @@
-import {defineRouting} from 'next-intl/routing';
-import {createNavigation} from 'next-intl/navigation';
- 
+import { defineRouting } from 'next-intl/routing';
+import { createNavigation } from 'next-intl/navigation';
+
 export const routing = defineRouting({
   // A list of all locales that are supported
   locales: ['en', 'fr', 'es'],
- 
+
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+
+  // Always use locale prefix in URLs
+  localePrefix: 'always'
 });
- 
+
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
-export const {Link, redirect, usePathname, useRouter, getPathname} =
+export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);
